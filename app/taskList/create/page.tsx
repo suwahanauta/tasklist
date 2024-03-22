8b21080d0ba4a8a084a4d9ba1bfa84926b2bb23b
suwahanauta <suwa.hnut@gmail.com>
2024-03-22 20:10:14 +0900

    タスク作成画面のUI作成

--- app/taskList/create/page.tsx
+++ app/taskList/create/page.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 
-const tasks: string[] = ["リンゴ", "パイナップル", "ブドウ", "イチゴ", "ミカン", "バナナ"]
-
 export default function Home() {
     return (
-        <>
+        <div>
 
-            <div className="text-3xl text-center p-8 font-bold">
-                <h1>タスク作成画面</h1>
-            </div>
+            <h1 className="font-bold text-3xl m-8 text-center">タスク作成画面</h1>
 
-        </>
+            <input className="block w-1/5 h-16 text-neutral-950 rounded-md pr-4 pl-4 mt-8 mb-8 mx-auto"/>
+
+            <button className="block font-bold w-1/5 h-16 border-4 rounded-full mt-8 mx-auto">作成</button>
+
+        </div>
     );
 }
 
