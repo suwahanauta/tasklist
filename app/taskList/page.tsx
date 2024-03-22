ac2bed93c19487b3f514118d1707a59509673f94
suwahanauta <suwa.hnut@gmail.com>
2024-03-22 14:57:57 +0900

    ピクセルでサイズ指定している部分を変更

--- app/taskList/page.tsx
+++ app/taskList/page.tsx
@@ -6,38 +6,20 @@ export default function Home() {
     return (
         <>
 
-            <div className="text-[30px] text-center p-[30px] font-bold">
+            <div className="text-3xl text-center p-8 font-bold">
                 <h1>タスク一覧画面</h1>
             </div>
 
             <div>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[0]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[1]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[2]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[3]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[4]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[5]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[0]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[1]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[2]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[3]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[4]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[5]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[0]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[1]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[2]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[3]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[4]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[5]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[0]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[1]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[2]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[3]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[4]}</p>
-                <p className="border-2 text-center w-1/5 p-[10px] mx-auto m-[10px]">{tasks[5]}</p>
+                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[0]}</p>
+                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[1]}</p>
+                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[2]}</p>
+                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[3]}</p>
+                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[4]}</p>
+                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[5]}</p>
             </div>
 
-            <div className="text-[60px] border-4 w-[120px] h-[120px] flex items-center justify-center rounded-full font-mono right-[90px] fixed bottom-[90px]">
+            <div className="text-6xl border-4 w-28 h-28 flex items-center justify-center rounded-full font-mono right-20 fixed bottom-20">
                 <p>+</p>
             </div>
 
