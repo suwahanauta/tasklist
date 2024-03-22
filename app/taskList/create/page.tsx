516c68d929c523e4e6930678f6d21e696f534824
suwahanauta <suwa.hnut@gmail.com>
2024-03-22 15:50:24 +0900

    作成画面のセグメントを追加

---
+++ app/taskList/create/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const tasks: string[] = ["リンゴ", "パイナップル", "ブドウ", "イチゴ", "ミカン", "バナナ"]
+
+export default function Home() {
+    return (
+        <>
+        aaa
+        </>
+    );
+}
+
