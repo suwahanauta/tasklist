dfe52598ab63a38df0952f05a4e90b7885dca3e7
suwahanauta <suwa.hnut@gmail.com>
2024-03-22 15:41:25 +0900

    map関数を用いてループ処理

--- app/taskList/page.tsx
+++ app/taskList/page.tsx
@@ -9,14 +9,13 @@ export default function Home() {
             <div className="text-3xl text-center p-8 font-bold">
                 <h1>タスク一覧画面</h1>
             </div>
-
+            
             <div>
-                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[0]}</p>
-                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[1]}</p>
-                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[2]}</p>
-                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[3]}</p>
-                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[4]}</p>
-                <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{tasks[5]}</p>
+                {tasks.map(
+                    (task) => {
+                        return <p className="border-2 text-center w-1/5 p-3 mx-auto m-3">{task}</p>
+                    }
+                )}
             </div>
 
             <div className="text-6xl border-4 w-28 h-28 flex items-center justify-center rounded-full font-mono right-20 fixed bottom-20">
