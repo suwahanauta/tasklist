891ae737e2d63da80b1311667d72c3903530b8d1
suwahanauta <suwa.hnut@gmail.com>
2024-03-22 22:38:03 +0900

    p要素からbutton要素に変更

--- app/taskList/page.tsx
+++ app/taskList/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
                 }
             )}
 
-            <p className="text-6xl border-4 w-28 h-28 flex items-center justify-center rounded-full font-mono right-20 fixed bottom-20">+</p>
+            <button className="text-6xl border-4 w-28 h-28 flex items-center justify-center rounded-full font-mono right-20 fixed bottom-20">+</button>
 
         </div>
     );
