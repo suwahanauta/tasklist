e12229029bdf50e86062331d4209d70795200ab0
suwahanauta <suwa.hnut@gmail.com>
2024-03-23 22:31:13 +0900

    ファンクション名と引数名の変更

--- app/taskList/create/page.tsx
+++ app/taskList/create/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
 
     const [text, setText] = useState("")
 
-    function textboxValue(e: any) {
-        setText(e.target.value)
+    function setState(inputElementInfo:any) {
+        setText(inputElementInfo.target.value)
     }
 
 
@@ -18,7 +18,7 @@ export default function Home() {
 
             <h1 className="font-bold text-3xl m-8 text-center">タスク作成画面</h1>
 
-            <input className="block w-1/5 h-16 text-neutral-950 rounded-md pr-4 pl-4 mt-8 mb-8 mx-auto" onChange={(e) => { textboxValue(e) }} />
+            <input className="block w-1/5 h-16 text-neutral-950 rounded-md pr-4 pl-4 mt-8 mb-8 mx-auto" onChange={(inputElementInfo) => { setState(inputElementInfo) }} />
 
             <button className="block font-bold w-1/5 h-16 border-4 rounded-full mt-8 mx-auto" onClick={() => { console.log(text) }}>作成</button>
 
