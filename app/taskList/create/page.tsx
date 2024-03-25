bf8aacdbcff0461f53d0aca53c8f474ca16f26d5
suwahanauta <suwa.hnut@gmail.com>
2024-03-25 15:42:42 +0900

    useRefの変数名をわかりやすく

--- app/taskList/create/page.tsx
+++ app/taskList/create/page.tsx
@@ -6,16 +6,16 @@ import { useRef } from "react";
 
 export default function Home() {
 
-    const inputText = useRef<HTMLInputElement>(null)
+    const inputRef = useRef<HTMLInputElement>(null)
 
     return (
         <div>
 
             <h1 className="font-bold text-3xl m-8 text-center">タスク作成画面</h1>
 
-            <input ref={inputText} className="block w-1/5 h-16 text-neutral-950 rounded-md pr-4 pl-4 mt-8 mb-8 mx-auto" type="text" />
+            <input ref={inputRef} className="block w-1/5 h-16 text-neutral-950 rounded-md pr-4 pl-4 mt-8 mb-8 mx-auto" type="text" />
 
-            <button className="block font-bold w-1/5 h-16 border-4 rounded-full mt-8 mx-auto" onClick={() => { console.log(inputText.current?.value) }}>作成</button>
+            <button className="block font-bold w-1/5 h-16 border-4 rounded-full mt-8 mx-auto" onClick={() => { console.log(inputRef.current?.value) }}>作成</button>
 
         </div>
     );
