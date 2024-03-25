563661b7d7d79de209441400cf18efb7ee53f13e
suwahanauta <suwa.hnut@gmail.com>
2024-03-25 22:15:47 +0900

    tasksディレクトリを交換

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -6,7 +6,13 @@ export async function GET(request: Request) {
     const file = await promises.readFile(process.cwd() + '/app/api/data_tasks.json', 'utf8')
     const data = JSON.parse(file)
 
-    return new Response(data, {
+    const tasks = data.tasks
+
+    const response = JSON.stringify({
+        tasks: tasks,
+    })
+
+    return new Response(response, {
         status: 200,
     })
 }
