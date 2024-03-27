d6eb9fbcb54b8f7be86012d45ba0c43934125067
suwahanauta <suwa.hnut@gmail.com>
2024-03-27 14:24:11 +0900

    axiosをインストール

--- app/task/[id]/page.tsx
+++ app/task/[id]/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
     return (
         <div>
-            あいうえお
+            <h1 className="text-3xl text-center p-8 font-bold">タスク詳細画面</h1>
         </div>
     );
 }
